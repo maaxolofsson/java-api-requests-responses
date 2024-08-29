bd528a2f5ad1dd1009b6742b5cd0add28bdfde7b
Max V Olofsson <max.olofsson@se.experis.com>
2024-08-29 14:11:07 +0200

    Implemeneted create and getAll endpoint for language

--- src/main/java/com/booleanuk/api/requests/Languages.java
+++ src/main/java/com/booleanuk/api/requests/Languages.java
@@ -1,11 +1,30 @@
 package com.booleanuk.api.requests;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
+@RequestMapping("languages")
 public class Languages {
-    private List<Language> languages = new ArrayList<>(){{
+    private List<Language> languages = new ArrayList<>() {{
         add(new Language("Java"));
         add(new Language("C#"));
     }};
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Language create(@RequestBody Language language) {
+        this.languages.add(language);
+
+        return language;
+    }
+
+    @GetMapping
+    public List<Language> getAll() {
+        return this.languages;
+    }
+
 }
